d32c8e7ce9e0adda0285676990d027a7efcfba3f
ChristopherMora <95389955+ChristopherMora@users.noreply.github.com>
2023-01-29 23:24:07 -0600

    pantallas

---
+++ lib/actividad4/pantalla1.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/actividad4/pantalla2.dart';
+
+class FirstRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Route'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Open route'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondRoute()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
